f8f8e5d07a303837173612416edbe33afe8b4732
Juan Francisco Mosquera <maxiplux@gmail.com>
2019-04-19 11:58:57 -0500

    Initial commit

---
+++ src/main/java/springbatch/batchprocesor/BatchprocesorApplication.java
@@ -0,0 +1,13 @@
+package springbatch.batchprocesor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchprocesorApplication {
+    //https://spring.io/guides/gs/batch-processing/
+    public static void main(String[] args) {
+        SpringApplication.run(BatchprocesorApplication.class, args);
+    }
+
+}
